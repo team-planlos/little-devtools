77230274e15534f0393eb3560cc3e2079312af92
Johannes Loos <loos-johannes@gmx.de>
2023-03-04 22:55:39 +0000

    set up maturin

--- little-devtools/target/debug/deps/little_devtools-b0e26472214e661b.d
+++
@@ -1,5 +0,0 @@
-/workspaces/little-devtools/little-devtools/target/debug/deps/little_devtools-b0e26472214e661b.rmeta: src/main.rs
-
-/workspaces/little-devtools/little-devtools/target/debug/deps/little_devtools-b0e26472214e661b.d: src/main.rs
-
-src/main.rs:
---
+++ little_devtools/examples/examples.py
@@ -0,0 +1,4 @@
+import word_counter
+
+result = word_counter.sum_as_string(1, 2)
+print(result)
