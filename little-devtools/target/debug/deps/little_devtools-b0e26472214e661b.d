64342269d3e4680621cf6c250efd335ca0444781
Johannes Loos <loos-johannes@gmx.de>
2023-03-04 20:43:59 +0000

    compile

---
+++ little-devtools/target/debug/deps/little_devtools-b0e26472214e661b.d
@@ -0,0 +1,5 @@
+/workspaces/little-devtools/little-devtools/target/debug/deps/little_devtools-b0e26472214e661b.rmeta: src/main.rs
+
+/workspaces/little-devtools/little-devtools/target/debug/deps/little_devtools-b0e26472214e661b.d: src/main.rs
+
+src/main.rs:
---
+++ little-devtools/target/debug/deps/little_devtools-ce35c8a37c5fa1f6.d
@@ -0,0 +1,5 @@
+/workspaces/little-devtools/little-devtools/target/debug/deps/little_devtools-ce35c8a37c5fa1f6.rmeta: src/main.rs
+
+/workspaces/little-devtools/little-devtools/target/debug/deps/little_devtools-ce35c8a37c5fa1f6.d: src/main.rs
+
+src/main.rs:
