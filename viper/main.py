43c19251c9a9630bba2ac04bc663e8c77c69ad70
Johannes Loos <jojoi7@gmx.de>
2023-04-24 16:18:44 +0200

    changed to 1 sensor

--- viper/main.py
+++ viper/main.py
@@ -13,33 +13,6 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
 # # Create your objects here.
-# ev3 = EV3Brick()
-# CL = ColorSensor(Port.S2)
-# CR = ColorSensor(Port.S3)
-
-# ML = Motor(Port.C)
-# MR = Motor(Port.B)
-
-# db = DriveBase(ML, MR, 88, 88)
-
-
-# print('links: ')
-# print(CL.reflection())
-# print('rechts: ')
-# print(CR.reflection())
-
-# while True:
-#     if CR.reflection() < 40:
-#         db.turn(-2)
-#     elif CL.reflection() < 40:
-#         db.turn(2)
-#     else:
-#         db.straight(30)
-#     print('links: ')
-#     print(CL.reflection())
-#     print('rechts: ')
-#     print(CR.reflection())
-#     wait(10)
 
 ev3 = EV3Brick()
 ML = Motor(Port.C)
